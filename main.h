99b651d406bc9615a5915b4b881e357d872889a5
Amoraaaa <ammarking503@gmail.com>
2023-08-21 06:26:17 -0700

    main

---
+++ main.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * include all used libraries
+ * 
+*/
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * define used macros
+*/
+#define BUFFER_SIZE 1024
+#define NULL_STRING "(null)"
+/**
+ * _printf - format like printf
+ * @format: format array string
+ * 
+ * Return: counter of chars
+*/
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_charcter(va_list ptr);
+int print_string(va_list ptr);
+int print_percent(va_list ptr);
+int _strlen(char *s);
+int print_decimal(va_list ptr);
+int print_binary(va_list ptr);
+int print_hexadecimal(va_list ptr, int lowercase);
+int print_hex_upper_X(va_list ptr);
+int print_hex_lower_x(va_list ptr);
+int print_octal(va_list ptr);
+int print_unsigned(va_list ptr);
+
+
+/**
+ * specifiers - struct ...
+ * @specifier_id: ...
+ * @f: ...
+*/
+
+typedef struct specifiers {
+    char *specifier_id;
+    int (*f)(va_list);
+}specifiers_format;
+
+#endif
