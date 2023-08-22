578edc923519b5bc5a21d3abe5505280525be5da
Amoraaaa <ammarking503@gmail.com>
2023-08-22 10:29:41 -0700

    XYZ

---
+++ print_rot13_string.c
@@ -0,0 +1,35 @@
+#include "main"
+
+/**
+ * print_rot13_string - ....
+ * @ptr: ...
+ * Return: ...
+ */
+
+int print_rot13_string(va_list ptr)
+{
+	char *str;
+	char base;
+	int len = 0, i = 0;
+
+	str = va_arg(ptr, char *);
+
+	/** check if null string */
+
+	if (!str)
+		return _puts(NULL_STRING);
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			base = (str[i] >= 'a' && str[i] <= 'z') ? 'a' : 'A';
+			_putchar((str[i] - base + 13) % 26 + base);
+		}
+		else
+			_putchar(str[i]);
+		i++;
+		len++;
+	}
+	return (len);
+}
